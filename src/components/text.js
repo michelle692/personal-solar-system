d1c9f18ebd2c0c426a8630f4a1e6529fbf7b0c68
michelle <michelle_lee100@hotmail.com>
2024-03-23 00:16:01 -0400

    bug fix

--- src/components/text.js
+++ src/components/text.js
@@ -4,8 +4,8 @@ function Text({ onClick, text, positionTop, isSelected }) {
 
   return (
     <div className={className} style={{ position: 'absolute', top: `${positionTop}px`, left: '20px', fontSize: '12px', fontWeight: 'bold' }}>
-      <div onClick={onClick} style={{ cursor: 'pointer' }}>{text}</div>
-      <div style={{ height: '20px', cursor: 'default', pointerEvents: 'none' }}></div>
+      <div onClick={onClick} style={{ cursor: 'pointer' }}>{text} </div>
+      <div style={{ height: '20px', cursor: 'default', pointerEvents: 'none' }}> </div>
     </div>
   );
 }
